946294715323c198d7ae360ae768188206f026d3
Industrial Brain <106162109+IndustrialBrain@users.noreply.github.com>
2022-05-24 16:02:13 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import  Flask, request, jsonify, json
+import requests
+from pyModbusTCP.client import ModbusClient
+import time
+
+# Inicia configuração TCP
+c = ModbusClient(host="localhost", port=502, unit_id=1, auto_open=True)
+# Parametros conexão
+c = ModbusClient()
+c.host("localhost")
+c.port(502)
+c.unit_id(1)
+app = Flask(__name__)
+
+#Rota para leitura
+@app.route("/read", methods = ['POST'])
+def leitura():
+     # Abre conexão TCP
+    c.open()
+
+    #Leitura de 3 registros a partir do 40001
+    regs = c.read_holding_registers(0, 3)
+    if regs:
+        print(regs)
+        #Fecha conexão    
+        c.close()
+        return jsonify({"40001":regs[0], "40002":regs[1], "40003":regs[2]} )
+    else:
+        print("read error")
+        #Fecha conexão    
+        c.close()
+        return "Erro na leitura" 
+
+@app.route("/write", methods = ['POST'])
+def escrita():
+    # Valores recebidos
+    valores = json.loads(request.data)
+
+     # Abre conexão TCP
+    c.open()
+
+   #Escreve em 3 registros a partir do 40004. Escrita apenas para demonstrar.
+    if c.write_multiple_registers(3, [valores["40004"],valores["40005"],valores["40006"]]):
+        print("write ok")
+        #Fecha conexão    
+        c.close()
+        return "escrita OK"
+    else:
+        print("write error")
+        #Fecha conexão    
+        c.close()
+        return "escrita OK"
+    
+    
+    
